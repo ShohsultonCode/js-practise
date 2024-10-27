ae904f3446d95e52708453c51c65f2536a91b63e
ShohsultonDev <shohsulton.198@gmail.com>
2024-10-27 19:41:16 +0500

    add: all type of javascript funtions

---
+++ functions.js
@@ -0,0 +1,53 @@
+//this is funtion declaration !
+// function sayMyName(name) {
+//     console.log("MR.", name);
+// }
+// sayMyName("Shohsulton")
+
+
+//funtion Expression
+// const sayMyName = function (name) {
+//     console.log("Mr", name);    
+// }
+// sayMyName("Shohsulton")
+
+
+// //Arrow Funtions
+// const calculate = (a, b) => a + b;
+// console.log(calculate(5, 8));
+
+
+//Anonymous functions
+// setTimeout(function(){
+//     console.log("this is fucking anonymous funtions");
+// }, 1000)
+
+
+//Nested Functions
+// function outFuntion(data) {
+//     function insideFuntion() {
+//         console.log(`Inside funtion. data is: ${data}`);
+//     }
+//     insideFuntion()
+// }
+// outFuntion("fuck girls")
+
+
+// IIFE Immediately Invoked funotion Expression 
+// (function (){
+//     console.log("fuck yout bitch")
+// })();
+
+
+
+//Async funtion
+// async function firstDataGet() {
+//     let result = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//     let data = await result.json();
+//     console.log(data);
+// }
+
+// firstDataGet();
+
+
+
